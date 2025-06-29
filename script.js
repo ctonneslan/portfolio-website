769a375b975f3a056bbf7e20b49eb390a9c9971f
Charlie Tonneslan <cst0520@gmail.com>
2025-06-29 18:02:33 -0400

    First draft

---
+++ script.js
@@ -0,0 +1,7 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message sent! Thank you for reaching out.");
+    this.reset();
+  });
